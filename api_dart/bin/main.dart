af5d6d6234fb5cda5a3385031f533c797977f127
alisu99 <alissonsts910@gmail.com>
2025-06-28 22:54:19 -0300

    ...

--- api_dart/bin/main.dart
+++ api_dart/bin/main.dart
@@ -1,4 +1,5 @@
 import 'package:http/http.dart';
+import 'dart:convert';
 
 void main() {
   requestData();
@@ -13,11 +14,17 @@ void main() {
 //   },);
 // }
 
-requestData () {
-  String url = "https://gist.githubusercontent.com/alisu99/3b7876f83bec6ce55e629951b77f5d78/raw/211e8ef4c7f5acc7b92c99c0e04d505b0cba294b/gistfile1.txt";
+requestData() {
+  String url =
+      "https://gist.githubusercontent.com/alisu99/4d857ec0df68c4b8514e3af26fe795f5/raw/73b1e6247f2f3c6905afd67b78c3614439eadc93/gistfile1.txt";
 
   Future<Response> resposta = get(Uri.parse(url));
   resposta.then((Response resposta) {
     print(resposta.body);
-  },);
+    List<dynamic> listaAccounts = json.decode(resposta.body);
+    Map<String, dynamic> contaCarla = listaAccounts.firstWhere(
+      (element) => element["name"] == "Carla",
+    );
+    print(contaCarla["balance"]);
+  });
 }
