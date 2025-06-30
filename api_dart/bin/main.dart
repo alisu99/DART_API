70d4bedbbe3d93ffe8f2bec53caa1d5dd8f9acc6
Alisson Alencar <alissonsts910@gmail.com>
2025-06-30 17:57:25 -0300

    ...

--- api_dart/bin/main.dart
+++ api_dart/bin/main.dart
@@ -2,7 +2,8 @@ import 'package:http/http.dart';
 import 'dart:convert';
 
 void main() {
-  requestData();
+  // requestData();
+  requestDataAsync();
 }
 
 // requestData() {
@@ -27,4 +28,12 @@ requestData() {
     );
     print(contaCarla["balance"]);
   });
+}
+
+
+requestDataAsync() async {
+  String url = "https://gist.githubusercontent.com/alisu99/4d857ec0df68c4b8514e3af26fe795f5/raw/73b1e6247f2f3c6905afd67b78c3614439eadc93/gistfile1.txt";
+
+  Response response = await get(Uri.parse(url));
+  print(json.decode(response.body)[0]);
 }
